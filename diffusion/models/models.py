7545c71fafb8e329f589f3c8ba4d0c515028e214
Katherine Crowson <crowsonkb@gmail.com>
2022-01-04 12:46:08 -0800

    Add cc12m_1_cfg and its sampling script

--- diffusion/models/models.py
+++ diffusion/models/models.py
@@ -3,6 +3,7 @@ from . import cc12m_1, yfcc_1, yfcc_2
 
 models = {
     'cc12m_1': cc12m_1.CC12M1Model,
+    'cc12m_1_cfg': cc12m_1.CC12M1Model,
     'yfcc_1': yfcc_1.YFCC1Model,
     'yfcc_2': yfcc_2.YFCC2Model,
 }
